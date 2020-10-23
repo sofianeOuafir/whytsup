4a5b1323f20287168c20d461931bfe29d47adb09
Sofiane <sofiane.ouafir@live.fr>
2020-10-23 12:56:49 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://wu-wd-interface.herokuapp.com']
+    origins ['http://localhost:3000', 'https://wu-wd-interface.herokuapp.com', 'http://www.buyingforselling.com']
 
     resource '*',
       headers: :any,
